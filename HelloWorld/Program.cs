e8a19a81a35fb2e95b55d8b78d52cdd751889b3e
Alexander Karppi Säterö <alexander.karppisatero@elev.ga.ntig.se>
2024-08-26 14:31:50 +0200

    Tränat på cw, cr och svenska tecken

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("Hej, världen!");
+Console.WriteLine("Vad heter du?");
+string namn = Console.ReadLine();
+Console.WriteLine("Hej " + namn);
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Blue;
+
+Console.WriteLine("Vad gör du på fritiden?");
+string fritid = Console.ReadLine();
+Console.WriteLine("Jag tycker också om " + fritid);
