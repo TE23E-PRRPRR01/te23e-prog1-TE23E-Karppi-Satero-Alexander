a50be5f91915d0efdf244ecd39a73d36fdf836e8
Alexander Karppi Säterö <alexander.karppisatero@elev.ga.ntig.se>
2024-09-02 14:52:25 +0200

    Vi har övat på if else satser

---
+++ Kapitel-3/EngChart/Program.cs
@@ -0,0 +1,84 @@
+﻿// Program som simulerar kända Engineering Flowchart
+Console.Clear();
+Console.WriteLine("Välkommen till ingenjörens flödeschema");
+
+// Visa en ASCii art
+Console.WriteLine($"""
+                                          $"   *.      
+              d$$$$$$$P"                  $    J
+                  ^$.                     4r  "
+                  d"b                    .db
+                 P   $                  e" $
+        ..ec.. ."     *.              zP   $.zec..
+    .^        3*b.     *.           .P" .@"4F      "4
+  ."         d"  ^b.    *c        .$"  d"   $         %
+ /          P      $.    "c      d"   @     3r         3
+4        .eE........$r===e$$$$eeP    J       *..        b
+$       $$$$$       $   4$$$$$$$     F       d$$$.      4
+$       $$$$$       $   4$$$$$$$     L       *$$$"      4
+4         "      ""3P ===$$$$$$"     3                  P
+ *                 $                  b                J
+  ".             .P                    %.             @
+    %.         z*"                      ^%.        .r"
+       "*==*""                             ^"*==*""   
+""");
+
+// Flödesschema
+
+Console.Write("Does it move? (y/n) ");
+string answer = Console.ReadLine();
+
+// Kolla om svar är ja eller nej
+if (answer == "y")
+{
+    Console.Write("Should it? (y/n) ");
+    answer = Console.ReadLine();
+    if (answer == "y")
+    {
+        Console.WriteLine("No problem!");
+    }
+    else
+    {
+        Console.WriteLine("Plz use Duct Tape");
+    }
+}
+else
+{
+    Console.Write("Should it? (y/n) ");
+    answer = Console.ReadLine();
+    if (answer == "y")
+    {
+        Console.WriteLine("Plz use WD40");
+    }
+    else
+    {
+        Console.WriteLine("No problem!");
+    }
+}
+
+// Annan variant av samma flödesschema
+
+Console.Write("Does it move? (y/n)");
+string svar = Console.ReadLine();
+Console.Write("Should it? (y/n) ");
+string svar2 = Console.ReadLine();
+if ((svar == "y" && svar2 == "y"))
+{
+    Console.WriteLine("No problem! ");
+}
+else if (svar == "n" && svar2 == "y")
+{
+    Console.WriteLine("Plz use WD40");
+}
+else if (svar == "y" && svar2 == "n")
+{
+    Console.WriteLine("Plz use Duct Tape");
+}
+else if (svar == "n" && svar2 == "n")
+{
+    Console.WriteLine("No problem! ");
+}
+else 
+{
+    Console.WriteLine("Answer with y/n");
+}
---
+++ Kapitel-3/InteractivStory/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO.Pipes;
+using System.Linq.Expressions;
+
+Console.Clear();
+Console.Write("Du befinner dig i en korridor. Vilket håll går du (N/V/S)");
+string väderstreck = Console.ReadLine();
+
+if (väderstreck == "N")
+{//ifall man svarar N
+    Console.Write("Du går norr och möter en katt. Klappa katten? (J/N)");
+    string Klappa = Console.ReadLine();
+    
+    if (Klappa == "J")
+    {
+        Console.WriteLine("Du klappar katten");
+    }
+    else
+    {
+        Console.WriteLine("Du blir uppäten av katten :)");
+    }
+}
+else if (väderstreck == "V")
+{//ifall man svarar V
+    Console.WriteLine("Du går väst, ");
+}
+else if (väderstreck == "S")
+{//ifall man svarar S
+    Console.WriteLine("Du går syd, ");
+}
+else 
+{//ifall man svarar något annat
+    Console.WriteLine("Du dog :(");
+}
