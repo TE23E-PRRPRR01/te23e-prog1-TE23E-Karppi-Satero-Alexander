8d8858bde6c20bec6c3849925b66d0942d2e2325
Alexander Karppi Säterö <alexander.karppisatero@elev.ga.ntig.se>
2024-09-09 14:50:28 +0200

    Vi har övat på konvertering med int.Pars

---
+++ Kapitel-3/AldersGranser/Program.cs
@@ -0,0 +1,26 @@
+﻿// Exempel på spel som man får spela för sin ålder
+
+Console.Clear();
+Console.WriteLine("Få exempel på spel till din ålder");
+
+Console.Write("Ange din ålder: ");
+string ålderText = Console.ReadLine();
+//Konvertera texten till ett heltal
+int ålder = int.Parse(ålderText);
+
+if (ålder >= 12 && ålder < 18)
+{
+    Console.WriteLine("Gå i skolan och plugga istället för att spela");
+}
+else if (ålder >= 18)
+{
+    Console.WriteLine("Spela vad du vill, jag vet inte");
+}
+else if (ålder <= 3)
+{
+    Console.WriteLine("Spela inte överhuvudtaget");
+}
+else if (ålder > 3 && ålder < 12)
+{
+    Console.WriteLine("Spela Minecraft eller Roblox");
+}
---
+++ Kapitel-3/GissaTalet/Program.cs
@@ -0,0 +1,28 @@
+﻿// Ett litet spel - gissa ett hemligt heltal
+using System.Security.Cryptography;
+
+Console.Clear();
+Console.WriteLine("Ett litet spel - gissa ett hemligt heltal");
+
+// Slumpa ett tal
+int slumpTalet = Random.Shared.Next(1, 101); // 1-100
+Console.WriteLine($"Slumptalet är {slumpTalet}");
+
+Console.Write("Gissa ett tal 1-100 ");
+// Läs in texten, t.ex. "100"
+string gissningText = Console.ReadLine();
+// Konvertera texten till ett heltal, blir 100
+int gissning = int.Parse(gissningText);
+
+if (gissning == slumpTalet)
+{
+    Console.WriteLine("Bra, du har gissat rätt!");
+}
+else if (gissning > slumpTalet)
+{
+    Console.WriteLine("Gissningen är för hög");    
+}
+else
+{   
+    Console.WriteLine("Gissningen är för låg");
+}
