d9bbf7a14d0b38eb0c78557a31d513647df6d2c1
Alexander Karppi Säterö <alexander.karppisatero@elev.ga.ntig.se>
2024-11-06 14:56:38 +0100

    Övat på att skapa en lista med slumpade tal

---
+++ Kapitel-5/SlumpadLista/Program.cs
@@ -0,0 +1,30 @@
+﻿// Slumpa många tal och spara i lista
+Console.Clear();
+Console.WriteLine("Slumpa många tal och spara i lista");
+
+// Globala variabler 
+// En lista för heltat
+List<int> listaSlumptal = [];
+
+// Be användare ange antal slumpade tal
+Console.Write("Ange antal slumptal: ");
+int antalSlumptal = int.Parse(Console.ReadLine());
+
+// Be användare ange min och max värde
+Console.Write("Ange minsta värde för slumptal: ");
+int minTal = int.Parse(Console.ReadLine());
+Console.Write("Ange största värde för slumptal: ");
+int maxTal = int.Parse(Console.ReadLine());
+
+// Loopa 5 ggr 
+for (int i = 0; i < antalSlumptal; i++)
+{
+    // Slumpa ett tal min-max
+    int slumptal = 0;
+    slumptal = Random.Shared.Next(minTal, maxTal + 1);
+
+    // Lägg till slumptalet i listan
+    listaSlumptal.Add(slumptal);
+
+    Console.WriteLine($"Slumpat tal {i + 1}: {slumptal}");
+}
