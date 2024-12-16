5689474da6b465ad1ffa6940305ed40366a4f9a7
Alexander Karppi Säterö <alexander.karppisatero@elev.ga.ntig.se>
2024-12-16 14:48:19 +0100

    Klar med mikroövningar

---
+++ Kapitel-5/Listor2/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+Console.WriteLine("Allt om listor");
+
+// Skapa en tom lista
+List<string> listaNamn = [];
+
+// Infoga i en lista 
+listaNamn.Add("Alex");
+listaNamn.Add("Alexander");
+Console.WriteLine(listaNamn.Count);
+
+// Skriv ut lista 
+foreach (var namn in listaNamn)
+{
+    Console.WriteLine($"- {namn}");
+}
+
+// skriv ut första elementet i listan 
+Console.WriteLine($"Första elementet: {listaNamn[0]}");
+
+// Skriv ut två namn ur listan
+
+for (var i = 0; i < 2; i++)
+{
+    Console.WriteLine($"Första elementet: {listaNamn[i]}");
+}
+
+// Skriv ut all namn i listan med for-loop
+for (var i = 0; i < listaNamn.Count; i++)
+{
+    Console.WriteLine($"{i}: {listaNamn[i]}");
+}
---
+++ Kapitel-5/MikroOvningar4/Program.cs
@@ -0,0 +1,69 @@
+﻿Console.Clear();
+
+// Skapa listor
+List<string> listaActionSpel = [];
+List<string> listaÄventyrsSpel = [];
+
+while (true)
+{
+    Console.Write("""
+
+    a. Visa alla action-spel
+    b. Registrera ett action-spel
+    c. Visa alla äventyrsspel
+    d. Registrera tre äventyrsspel
+    e. Avsluta
+    Välj ett alternativ:
+    """);
+
+    string val = Console.ReadLine();
+    if (val == "a")
+    {
+        // Visa alla actionspel
+        Console.Clear();
+        Console.WriteLine("Här är alla action-spel i listan:");
+        foreach (var actionSpel in listaActionSpel)
+        {
+            Console.WriteLine($"- {actionSpel}");
+        }
+    }
+    else if (val == "b")
+    {
+        // Registrera nytt actionspel
+        Console.Clear();
+        Console.Write("Ange ett action-spel: ");
+        listaActionSpel.Add(Console.ReadLine());
+    }
+    else if (val == "c")
+    {
+        // Visa alla äventyrsspel
+        Console.Clear();
+        Console.WriteLine("Här är alla äventyrsspel i listan:");
+        foreach (var äventyrsSpel in listaÄventyrsSpel)
+        {
+            Console.WriteLine($"- {äventyrsSpel}");
+        }
+    }
+    else if (val == "d")
+    {
+        // Registera tre nya äventyrsspel
+        Console.Clear();
+        Console.WriteLine("Lägg till tre äventyrsspel");
+        for (var i = 0; i < 3; i++)
+        {   
+        Console.Write("Ange ett äventyrsspel: ");
+        listaÄventyrsSpel.Add(Console.ReadLine());
+        }
+    }
+    else if (val == "e")
+    {
+        // Avsluta
+        break;
+    }
+    else
+    {
+        // Felmeddelande vid ogilitig input
+        Console.Clear();
+        Console.WriteLine("Välj ett av alternativen");
+    }
+}
