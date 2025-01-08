28b57a19469a0f3c579c15baf60d84af79b1e492
Alexander Karppi Säterö <alexander.karppisatero@elev.ga.ntig.se>
2025-01-08 15:03:24 +0100

    Tränat på metoder

---
+++ Kapitel-6/EnMassaMetoder/Program.cs
@@ -0,0 +1,38 @@
+﻿/* ****************************************************************
+*                            M A I N                              *
+*******************************************************************/
+
+Console.Clear();
+Console.WriteLine("Hello, World!");
+
+SägHejNamn("Valentin");
+SägHejNamn("Johnny");
+SägHejNamn("Benjamin");
+SägHejNamn("");
+
+TalIKvadrat(1);
+TalIKvadrat(2);
+TalIKvadrat(3);
+TalIKvadrat(4);
+
+/* ****************************************************************
+*                            M E T O D E R                        *
+*******************************************************************/
+
+/// <summary>
+/// Skriv ut häslning
+/// </summary>
+/// <param name="namn">Namnet</param>
+static void SägHejNamn(string namn)
+{
+    Console.WriteLine($"Hej på dig {namn}!");
+}
+
+/// <summary>
+/// Skriver ut tal i kvadrat
+/// </summary>
+/// <param name="tal">Talet</param>
+static void TalIKvadrat(int tal)
+{
+    Console.WriteLine(tal * tal);
+}
---
+++ Kapitel-6/MetoderIntro/Program.cs
@@ -0,0 +1,52 @@
+﻿/* ****************************************************************
+*                            M A I N                              *
+*******************************************************************/
+
+Console.Clear();
+Console.WriteLine("Hej metoder!");
+
+// Anropa/använd metoden
+SägHej();
+SägHej();
+SägHej();
+SägHej();
+SägHej();
+
+// Anropa med metoden meny
+VisaMeny();
+
+// Anropa metoden avslutar
+SägAvslutar();
+
+/* ****************************************************************
+*                            M E T O D E R                        *
+*******************************************************************/
+
+/// <summary>
+/// Metod som skriver ut en hälsning
+/// </summary>
+static void SägHej()
+{
+Console.WriteLine("Hej på dig!");
+}
+
+/// <summary>
+/// Metod som skriver ut en meny
+/// </summary>
+static void VisaMeny()
+{
+    Console.WriteLine("""
+    1. Lägg till uppgift
+    2. Ta bort uppgift
+    3. Skriv ut alla uppgifter
+    4. Avsluta
+    """);
+}
+
+/// <summary>
+/// Skriver ut avslutar
+/// </summary>
+static void SägAvslutar()
+{
+    Console.WriteLine("... Avslutar ...");
+}
